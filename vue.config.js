d497a5d94340aff9387cdc86e9a4cf99095f1ab1
Henrique Souzo <hsprograma@gmail.com>
2023-03-16 23:06:46 -0300

    commit

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,4 @@ const { defineConfig } = require('@vue/cli-service')
 }) */
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/production-sub-path/'
-    : '/'
-}
+  publicPath: '/curso-html/' }
